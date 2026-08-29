f6984a8c5afb2a3c7adc88be4539863fd8a922d6
humayranuha <nuhahumayra24@gmail.com>
2026-08-29 13:51:50 +0600

    stack implemented with pure c++

--- stack.cpp
+++ stack.cpp
@@ -1,96 +1,75 @@
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
-#define MAX 10  
-
-// Creating a stack
-struct stack
+class Stack
 {
-    int items[MAX];
-    int top;
+private:
+    vector<int> items;
+    const int MAX = 10;
+
+public:
+    bool isFull() const
+    {
+        return items.size() == MAX;
+    }
+
+    bool isEmpty() const
+    {
+        return items.empty();
+    }
+
+    void push(int newItem)
+    {
+        if (isFull())
+        {
+            cout << "Stack is full!!!" << endl;
+        }
+        else
+        {
+            items.push_back(newItem);
+        }
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty!!!" << endl;
+        }
+        else
+        {
+            cout << "Item popped= " << items.back() << endl;
+            items.pop_back();
+        }
+    }
+
+    void print() const
+    {
+        cout << "Stack: ";
+        for (int item : items)
+        {
+            cout << item << " ";
+        }
+        cout << endl;
+    }
 };
 
-void createEmptyStack(stack *s)
+int main()
 {
-    s->top = -1;
-}
+    Stack s;
 
-// Check if the stack is full
-int isFull(stack *s)
-{
-    if (s->top == MAX - 1)
-        return 1;
-    else
-        return 0;
-}
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
 
-// Check if the stack is empty
-int isEmpty(stack *s)
-{
-    if (s->top == -1)
-        return 1;
-    else
-        return 0;
-}
+    s.print();
 
-// Add elements into stack
-void push(stack *s, int newItem)
-{
-    if (isFull(s))
-    {
-        cout << "Stack is full!!!" << endl;
-    }
-    else
-    {
-        s->top++;
-        s->items[s->top] = newItem;
-    }
-}
+    s.pop();
 
-// Remove element from stack
-void pop(stack *s)
-{
-    if (isEmpty(s))
-    {
-        cout << "Stack is empty!!!" << endl;
-    }
-    else
-    {
-        cout << "Item popped= " << s->items[s->top] << endl;
-        s->top--;
-    }
-}
+    s.print();
 
-// Print elements of stack
-void printStack(stack *s)
-{
-    cout << "Stack: ";
-    for (int i = 0; i <= s->top; i++)  
-    {
-        cout << s->items[i] << " ";
-    }
-    cout << endl;
-}
-
-// Driver code
-int main() {
-    stack *s = (stack *)malloc(sizeof(stack));
-
-    createEmptyStack(s);
-
-    push(s, 1);
-    push(s, 2);
-    push(s, 3);
-    push(s, 4);
-
-    printStack(s);
-
-    pop(s);
-
-    printStack(s);
-
-    free(s);  // Clean up memory
     return 0;
 }
