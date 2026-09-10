30877ad91fd3a508f19d5da1b6f04ed34133a2e7
humayranuha <nuhahumayra24@gmail.com>
2026-09-10 14:17:30 +0600

    code indented for increasing readability

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -2,43 +2,54 @@
 #include <vector>
 using namespace std;
 
-void merge(vector<int>& arr, int left, int mid, int right) {
+void merge(vector<int> &arr, int left, int mid, int right)
+{
     int n1 = mid - left + 1;
     int n2 = right - mid;
     vector<int> L(n1), R(n2);
-    for (int i = 0; i < n1; i++) {
+    for (int i = 0; i < n1; i++)
+    {
         L[i] = arr[left + i];
     }
-    for (int j = 0; j < n2; j++) {
+    for (int j = 0; j < n2; j++)
+    {
         R[j] = arr[mid + 1 + j];
     }
 
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        }
+        else
+        {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
+void mergeSort(vector<int> &arr, int left, int right)
+{
+    if (left < right)
+    {
         int mid = left + (right - left) / 2;
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
@@ -46,14 +57,17 @@ void mergeSort(vector<int>& arr, int left, int right) {
     }
 }
 
-void printArray(const vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
+void printArray(const vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {12, 11, 13, 5, 6, 7};
     int arr_size = arr.size();
 
