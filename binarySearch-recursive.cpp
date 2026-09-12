c2c0f52572efccc010faae0515e758373dcf0da1
humayranuha <nuhahumayra24@gmail.com>
2026-09-12 10:36:39 +0600

    recursive binary search implemented

---
+++ binarySearch-recursive.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int x, int low, int high) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (array[mid] == x) 
+            return mid;
+        else if (array[mid] < x) 
+            return binarySearch(array, x, mid + 1, high);
+        else 
+            return binarySearch(array, x, low, mid - 1);
+    }
+    return -1;
+}
+
+int main() {
+    int array[] = {3, 4, 5, 6, 7, 8, 9};
+    int x = 4;
+    int n = sizeof(array) / sizeof(array[0]);
+
+    int result = binarySearch(array, x, 0, n - 1);
+
+    (result == -1) ? cout << "Element not found" : cout << "Element found at index: " << result;
+}
